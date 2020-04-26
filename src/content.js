f4c3f8a8267bfc8631fcfe53531a584a886ead91
piervito <piervito@MBP-di-piervito.station>
2020-04-26 15:41:39 +0200

    First Commit

---
+++ src/content.js
@@ -0,0 +1,147 @@
+// content.js
+const checkCallOn = () => {
+    let menu = document.getElementsByClassName('Jrb8ue')
+    if (menu.length > 0) { // If the menu exists we are in a call
+        clearInterval(intervalId) // Stop the loop
+        main() // Call the main function
+    }
+    
+}
+
+var intervalId = setInterval(checkCallOn, 250) // Start a loop until a call is entered
+var timerId = null
+var timerNode = null
+const main = () => {
+    const s = document.createElement('style')
+    s.innerHTML = style
+    document.body.append(s)
+
+    timerNode = document.createElement('div')
+    timerNode.innerHTML = timerHtml
+    document.body.append(timerNode);
+
+   /* Could be used to develop a pause function
+    timerNode.addEventListener('click', () => {
+        sessionStorage.setItem('seconds', 'value');
+    })*/
+
+    chrome.storage.sync.get(['seconds'], function(result) {
+        if (result.seconds) {
+            timer(result.seconds)
+        } else {
+            document.getElementById('time').innerHTML = "Non impostato"
+            setTimeout(() => timerNode.style.display = 'none', 60000)
+        }
+    });
+
+    chrome.storage.onChanged.addListener(function(changes) {
+        if (changes.seconds) {
+            if (timerId) {
+                clearInterval(timerId)
+                timerId = null
+            }
+            if (changes.seconds.newValue != 0) {                
+                if (document.getElementById('timer-banner')) {
+                    document.getElementById('timer-banner').outerHTML = ""
+                }                                
+                timer(changes.seconds.newValue)
+            } else {
+                document.getElementById('time').innerHTML = "Non impostato"
+                setTimeout(() => !timerId && (timerNode.style.display = 'none'), 60000)
+            }            
+        }        
+    });
+}
+
+const timer = (seconds) => {    
+    if (timerNode.style.display === "none") {
+        timerNode.style.display = 'block'
+    }
+    setTimer(seconds)
+    timerId = setInterval(function(){        
+        seconds--;
+        setTimer(seconds)
+        if (seconds < 0) {
+            clearInterval(timerId);
+            let parent = document.getElementsByClassName('o6gIdf zCbbgf')
+            if (parent.length) {
+                parent = parent[0]
+                const messageNode = document.createElement('div')
+                messageNode.setAttribute("style", "position: relative")
+                messageNode.innerHTML = message
+                parent.append(messageNode)
+                document.getElementById('timer-banner-button').addEventListener("click", () => document.getElementById('timer-banner').outerHTML = "")
+            } else {
+                console.log('[google-timer] Error: unable to show the finish message (no element by class)')
+            }        
+        }
+    }, 1000);
+}
+
+const setTimer = (seconds) => {
+    if (seconds >= 0) {
+        let hh = zeroFill(Math.floor(seconds / 3600))
+        let mm = zeroFill(Math.floor(seconds / 60) % 60)
+        let ss = zeroFill(Math.floor((seconds % 60)))
+    
+        if (hh > 0) {
+            document.getElementById('time').innerHTML = `${hh}:${mm}`
+        } else {
+            document.getElementById('time').innerHTML = `${mm}:${ss}`
+        }
+    }    
+}
+
+const zeroFill = (n) => {
+    return ('0'+n).slice(-2)
+}
+
+const timerHtml = `
+<div class="timer-body MCcOAc ">
+    <h3 class="text timer-title">Tempo Rimanente:</h3>
+    <div class="timer-container">
+        <span class="timer-digits text" id="time"></span>
+    </div>
+</div>
+`
+const style = `
+.timer-body {
+    background-color: black; 
+    width: fit-content;
+    padding: 5px 25px 5px 25px;
+    text-align: center;
+    border-radius: 5px;
+    height: fit-content;
+}
+.text {
+    color: white;
+}
+.timer-title {
+    font-size: 15px;
+    margin: 0;
+    opacity: 80%;
+}
+.timer-digits {
+    font-feature-settings: "tnum";
+    font-variant-numeric: tabular-nums;
+    font-size: 30px;
+    color: #3e4dec;
+}
+#timer-banner {
+    left: -110px;
+    top: -165px;
+}
+`
+
+const message = `
+<div class="MvD9Jd ncMv2e timer-banner" id="timer-banner" role="alert" aria-label="Stai parlando? Il microfono è disattivato.">
+    <div class="dj3AKc">
+        <p class="McXdrf">Il timer è scaduto! Vuoi chiudere la chiamata?</p>
+    </div>
+    <div role="button" id="timer-banner-button" class="uArJ5e Y5FYJe cjq2Db HZJnJ L8jh1" aria-label="Chiudi" aria-disabled="false" tabindex="0" data-tooltip="Chiudi" data-tooltip-vertical-offset="-12" data-tooltip-horizontal-offset="0">
+        <div class="PDXc1b MbhUzd" jsname="ksKsZd"></div><span jsslot="" class="XuQwKc"><span class="GmuOkf"><span class="DPvwYc tEYUvc" aria-hidden="true"><svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="CEJND cIGbvc NMm5M"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path></svg></span></span>
+        </span>
+    </div>
+    <div class="QSW55" jsname="nMj4jb" style="left: 125px;"></div>
+</div>
+`
---
+++ src/popup.js
@@ -0,0 +1,32 @@
+var button = document.getElementById('button')
+
+chrome.storage.sync.get(['hh', 'mm', 'ss'], function(result) {
+    console.log(result)
+    document.getElementById('hh').placeholder = result.hh
+    document.getElementById('mm').placeholder = result.mm
+    document.getElementById('ss').placeholder = result.ss
+  });
+
+document.addEventListener('keyup', (e) => {
+    console.log(e)
+    let hh = zeroFill(document.getElementById('hh').value)
+    let mm = zeroFill(document.getElementById('mm').value)
+    let ss = zeroFill(document.getElementById('ss').value)
+    let seconds = parseInt(hh)*3600 + parseInt(mm)*60 + parseInt(ss)
+    //button.innerHTML = "salvato"
+    console.log('ok', {hh, mm, ss, seconds})
+     // Save it using the Chrome extension storage API.
+     chrome.storage.sync.set({hh, mm, ss, seconds}, function() {
+        // Notify that we saved.
+        console.log('settings saved')
+      });
+})
+
+const zeroFill = (n) => {
+    if (n) {
+        return ('0'+n).slice(-2)
+    } else {
+        return "00"
+    }
+    
+}
