2ce5950a4e7fce8aa5d5d7f3d777d51b274a340b
Piervito <p.creanza@protonmail.com>
2020-05-02 22:41:44 +0200

    Improve graphic design and responsiveness

--- src/content.js
+++ src/content.js
@@ -1,7 +1,6 @@
 // content.js
 
 const socket = io.connect('https://timer.digitilab.it');
-
 // Interval functions
 const checkCallOn = () => {
     let menu = document.getElementsByClassName('Jrb8ue')
@@ -47,11 +46,27 @@ const checkCallOff = () => {
 
 const displayTimer = (bool) => {   
     if (bool) {
-        document.getElementById('google-timer').style.display = 'block'
+        document.getElementById('timer-container').style.display = 'flex'
     } else {
-        document.getElementById('google-timer').style.display = 'none'
+        document.getElementById('timer-container').style.display = 'none'
     }    
 }
+
+const displaySettings = (bool) => {
+    if (bool) {
+        document.getElementById('timer-settings-container').style.display = 'flex'
+        document.getElementById('timer-settings').style.display = 'none'
+        document.getElementById('timer-main-divider').classList.add('settings-open')        
+        document.getElementById('google-timer').classList.add('settings-open')
+
+    } else {
+        document.getElementById('timer-settings-container').style.display = 'none'
+        document.getElementById('timer-settings').style.display = 'block'
+        document.getElementById('timer-main-divider').classList.remove('settings-open')
+        document.getElementById('google-timer').classList.remove('settings-open')
+    }
+}
+
 
 var intervalId = setInterval(checkCallOn, 250) // Start a loop until a call is entered
 var checkCallOffInterval = null
@@ -60,16 +75,46 @@ var timeSet = null
 
 
 const main = (meetingId) => {
-    console.log('script on')
+    console.log('[google-timer] Plugin started!')
 
     document.body.insertAdjacentHTML('beforeend', style);
     document.body.insertAdjacentHTML('beforeend', timerHtml)
 
+    window.addEventListener('keyup', (e) => {
+        if (e.target.offsetParent.id === 'google-timer') {
+            clearInterval(timerInterval)
+            let hh = zeroFill(document.getElementById('hh').value)
+            let mm = zeroFill(document.getElementById('mm').value)
+            let ss = zeroFill(document.getElementById('ss').value)
+            let seconds = parseInt(hh)*3600 + parseInt(mm)*60 + parseInt(ss)
+            setTimer(seconds)
+
+             // Save it using the Chrome extension storage API.
+             chrome.storage.local.set({seconds});
+        }        
+    })
+    document.getElementById('timer-settings').addEventListener('mouseover', () => {
+        displaySettings(true)
+    })
+    document.getElementById('google-timer').addEventListener('mouseleave', () => {
+        displaySettings(false)
+    })
+
+    document.getElementById('timer-confirm').addEventListener('click', () => {
+        displaySettings(false)
+        chrome.storage.local.get('seconds', (result) => {
+            if (result.seconds) {
+                clearInterval(timerInterval)
+                timer(result.seconds)
+            }            
+          });
+    })
     chrome.storage.local.get(['seconds'], function(result) {
         if (result.seconds) {
             timer(result.seconds)
         } else {
-            document.getElementById('time').innerHTML = "Non impostato"
+            document.getElementById('time').style.color = "#5f6368"
+            document.getElementById('time').innerHTML = "00:00"
             setTimeout(() => !timer && displayTimer(false), 120000) // Disappear after 2 minutes
         }
     });
@@ -84,33 +129,13 @@ const main = (meetingId) => {
             console.log('too late')
         }
     })
-
-    chrome.storage.onChanged.addListener(function(changes) {
-        if (changes.seconds) {
-            clearInterval(timerInterval)
-            timerInterval = null
-            if (changes.seconds.newValue != 0) {                
-                if (document.getElementById('timer-banner')) {
-                    document.getElementById('timer-banner').outerHTML = ""
-                }
-                if (socket.connected) {
-                    socket.emit('sync_time', {id: meetingId, endTime: Date.now() + changes.seconds.newValue*1000})
-                } else {
-                    console.warn('[google-timer] Unable to sync time')
-                }
-                                                
-                timer(changes.seconds.newValue)
-            } else {
-                document.getElementById('time').innerHTML = "Non impostato"
-                setTimeout(() => !timerInterval && displayTimer(false), 60000)
-            } 
-        }   
-    });
 }
 
 const timer = (seconds) => {    
     displayTimer(true)    
     setTimer(seconds)
+    let msg = document.getElementById('timer-banner')
+    msg && (msg.outerHTML = '') // Remove end message if present
     timerInterval = setInterval(function(){        
         seconds--;
         setTimer(seconds)
@@ -126,7 +151,7 @@ const timer = (seconds) => {
                 parent.append(messageNode)
                 document.getElementById('timer-banner-button').addEventListener("click", () => document.getElementById('timer-banner').outerHTML = "")
             } else {
-                console.log('[google-timer] Error: unable to show the finish message (no element by class)')
+                console.log('[google-timer] Error: unable to show the finish timer-confirm (no element by class)')
             }        
         }
     }, 1000);
@@ -157,27 +182,88 @@ const zeroFill = (n) => {
     return ('0'+n).slice(-2)
 }
 //#d93025
+
+const settingsHtml = `
+<div class="timer-container settings" id="timer-settings-container">
+    <input class="timer-input" placeholder="00" name="time" id="hh" autocomplete="off"><p class="timer-label">h</p>
+    <div class="qO3Z3c timer-divider"></div>
+    <input class="timer-input" placeholder="00" name="time" id="mm" autocomplete="off"><p class="timer-label">min</p>
+    <div class="qO3Z3c timer-divider"></div>
+    <input class="timer-input" placeholder="00" name="time" id="ss" autocomplete="off"><p class="timer-label">sec</p>
+    <div class="qO3Z3c timer-divider"></div>
+    <div class="center">
+        <svg width="24" height="24" id="timer-close">
+            <path fill="green" d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"></path>
+        </svg>
+    </div>
+</div>
+`
+
+/*
+<svg width="24" height="24" id="timer-confirm">
+    <path fill="#5f6368" d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"></path>
+</svg>
+
+    <div class="qO3Z3c divider"></div>*/
 const timerHtml = `
-<div class="timer-app-container">
-    <div class="timer-body" id="google-timer" style="display: none;">
-    <p class="timer-digits text" id="time"></p>
+<div class="timer-app-container" id="timer-plugin">
+    <div class="timer-body" id="google-timer">
+        <div class="sub-container">
+            <div class="timer-container" id="timer-container">
+                <p class="timer-digits text" id="time"></p>
+                <div class="qO3Z3c divider" id="timer-main-divider"></div>
+                <span id="timer-settings" data-v-72ebea3c="" aria-hidden="true" class="DPvwYc sm8sCf SX67K">            
+                    <svg data-v-72ebea3c="" width="24" height="24" viewBox="0 0 24 24" focusable="false" class="Hdh4hc cIGbvc NMm5M">
+                        <path data-v-72ebea3c="" d="M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z"></path><circle data-v-72ebea3c="" cx="12" cy="12" r="3.5"></circle>
+                    </svg>
+                </span>
+            </div>
+            <div class="timer-container-settings" id="timer-settings-container" style="display: none;">
+                <input class="timer-input" placeholder="00" name="time" id="hh" autocomplete="off"><p class="timer-label">h</p>
+                <div class="qO3Z3c timer-divider"></div>
+                <input class="timer-input" placeholder="00" name="time" id="mm" autocomplete="off"><p class="timer-label">min</p>
+                <div class="qO3Z3c timer-divider"></div>
+                <input class="timer-input" placeholder="00" name="time" id="ss" autocomplete="off"><p class="timer-label">sec</p>
+                <div class="qO3Z3c timer-divider"></div>
+                <div class="center">
+                    <svg width="24" height="24" id="timer-confirm">
+                        <path fill="green" d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 `
 const style = `
 <style>
+.timer-divider {
+    margin-right: 10px;
+    margin-left: 10px;
+    height: 20px;
+}
+.divider {
+    margin-right: 10px;
+    margin-left: 10px;
+    height: 20px;
+}
 .timer-body {
+    display: flex;
     background-color: white; 
-    width: fit-content;
+    /*width: fit-content;*/
     border-radius: 0 0 8px 8px;
     padding: 0 20px 0 20px;
     text-align: center;
-    height: fit-content;
+    /*height: fit-content;*/
     top: 0;
     /*left: 0;*/
     position: absolute;
     z-index: 1;
     height: 48px;
+    max-width: 120px;
+    transition: height .5s ease-in-out, max-width .5s ease-in-out;
+    overflow: hidden;
+    flex-direction: column;
 }
 .text {
     color: white;
@@ -192,7 +278,7 @@ const style = `
     font-variant-numeric: tabular-nums;
     font-size: 30px;
     color: #5f6368;
-    margin: 5px 0 0 0;
+    margin: 0;
 }
 #timer-banner {
     left: -110px;
@@ -202,6 +288,80 @@ const style = `
     display: flex;
     justify-content: center;
 }
+.timer-container {
+    display: flex;
+    align-items: center;
+    height: 48px;
+}
+.timer-container-settings {
+    display: flex;
+    align-items: center;
+}
+.timer-input {
+    color: #5f6368;
+    font-weight: 600;
+    text-align: center;
+    width: 38px;
+    border: none;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 30px;
+    padding: 0;
+    background-color: transparent;
+    font-feature-settings: "tnum";
+    font-variant-numeric: tabular-nums;
+    margin-right: 5px;
+}
+.timer-label {
+    font-weight: 300;
+    font-size: 20px;
+    margin: 0;
+}
+.timer-input::placeholder {
+    color: #5f6368;
+    opacity: .5;
+}
+.timer-input:focus::placeholder { 
+    color: transparent;
+}
+.timer-body.settings-open {
+    max-width: 400px;
+}
+.sub-container {
+    display: flex; 
+    flex-wrap: wrap; 
+    width: 400px;
+}
+
+@media only screen and (max-width: 1080px) {
+    .timer-body.settings-open {
+        width: 120px;
+        height: 207px;
+    }
+    .timer-container-settings {
+        justify-content: flex-start;  
+        flex-wrap: wrap;      
+    }
+    .timer-input {
+        width: 50%;
+    }
+    .divider.settings-open, .timer-divider {
+        display: none;
+    }
+    #timer-confirm {
+        margin-bottom: 10px;
+        margin-top: 5px;
+    }
+    .center {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+    }
+    .sub-container {
+        justify-content: center;
+        width: inherit;
+    }
+  }
 </style>
 `
 
